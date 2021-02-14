dac139491d5f39ece5ad1c49423e53fb2f2148ba
student <student@eda-bc-06.localdomain>
2021-02-15 12:08:22 +1300

    added auth route

--- client/components/Authenticated.jsx
+++ client/components/Authenticated.jsx
@@ -1,20 +1,3 @@
-// import React from 'react'
-
-// // TODO: import a proper isAuthenticated function
-// const isAuthenticated = () => true
-
-// export function IfAuthenticated ({ children }) {
-//   return isAuthenticated()
-//     ? <>{ children }</>
-//     : null
-// }
-
-// export function IfNotAuthenticated ({ children }) {
-//   return !isAuthenticated()
-//     ? <>{ children }</>
-//     : null
-// }
-
 import React from 'react'
 import { isAuthenticated } from 'authenticare/client'
 
--- client/components/Register.jsx
+++ client/components/Register.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { register, isAuthenticated } from 'authenticare/client'
 
 import { baseApiUrl as baseUrl } from '../config'
 import { GridForm, ColOne, ColTwo, Button } from './Styled'
@@ -17,7 +18,15 @@ function Register (props) {
     })
   }
 
-  function handleClick () {
+  function handleClick (e) {
+    e.preventDefault()
+    const { username, password } = form
+    register({ username, password }, { baseUrl })
+      .then(() => {
+        if (isAuthenticated()) {
+          props.history.push('/')
+        }
+      })
   }
 
   return (
