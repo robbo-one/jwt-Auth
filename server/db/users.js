0e2749270419da49525471a1c700139318d49ff4
student <student@eda-bc-06.localdomain>
2021-02-15 12:33:20 +1300

    register working

--- server/db/users.js
+++ server/db/users.js
@@ -1,14 +1,9 @@
 const connection = require('./connection')
-
-module.exports = {
-  userExists,
-  getUserByName,
-  createUser
-}
+const { generateHash } = require('authenticare/server')
 
 function userExists (username, db = connection) {
   return db('users')
-    .count('id as n')
+  .count('id as n')
     .where('username', username)
     .then(count => {
       return count[0].n > 0
@@ -17,11 +12,26 @@ function userExists (username, db = connection) {
 
 function getUserByName (username, db = connection) {
   return db('users')
-    .select()
-    .where('username', username)
-    .first()
+  .select()
+  .where('username', username)
+  .first()
 }
 
 function createUser (user, db = connection) {
-  // you'll need to write this one
+  return userExists(user.username, db)
+    .then(exists => {
+      if (exists) {
+        return Promise.reject(new Error('User exists'))
+      }
+    })
+    .then(() => generateHash(user.password))
+    .then(passwordHash => {
+      return db('users').insert({ username: user.username, hash: passwordHash })
+    })
+}
+
+module.exports = {
+  userExists,
+  getUserByName,
+  createUser
 }
