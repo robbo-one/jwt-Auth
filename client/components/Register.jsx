190332d74b34d2329a7df8e4ef68edfcd485c4d1
Julia Kurganskaia <17925683+julia-kurganskaia@users.noreply.github.com>
2021-02-15 11:48:42 +1300

    Implement the handleClick function for the Register button

--- client/components/Register.jsx
+++ client/components/Register.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { register, isAuthenticated } from 'authenticare/client'
 
 import { baseApiUrl as baseUrl } from '../config'
 import { GridForm, ColOne, ColTwo, Button } from './Styled'
@@ -17,8 +18,16 @@ function Register (props) {
     })
   }
 
-  function handleClick () {
-  }
+  function handleClick (e) {
+    e.preventDefault();
+    const { username, password } = form;
+    register({ username, password })
+      .then(() => {
+        if (isAuthenticated()) {
+          props.history.push("/");
+        }
+      });
+  };
 
   return (
     <>
