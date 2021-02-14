5877293bc22c02adf580a7cc3189d1b2f0bb50f0
student <student@eda-bc-07.localdomain>
2021-02-15 11:32:48 +1300

    register and sign has got authentification working

--- client/components/Authenticated.jsx
+++ client/components/Authenticated.jsx
@@ -1,7 +1,29 @@
 import React from 'react'
 
+const localStorage = global.window.localStorage
+
+export const localStorageKeyName = 'token'
+
+
+// import { isAuthenticated } from 'authenticare'
+
 // TODO: import a proper isAuthenticated function
-const isAuthenticated = () => true
+export function isAuthenticated () {
+  const authToken = getToken()
+
+  if (authToken) {
+    const payload = decode(authToken)
+    const expiry = payload.exp
+
+    if (expiry < new Date().getTime() / 1000) {
+      logOff()
+      return false
+    }
+    return true
+  } else {
+    return false
+  }
+}
 
 export function IfAuthenticated ({ children }) {
   return isAuthenticated()
@@ -13,4 +35,16 @@ export function IfNotAuthenticated ({ children }) {
   return !isAuthenticated()
     ? <>{ children }</>
     : null
+}
+
+export function getToken (storage = localStorage) {
+  return storage.getItem(localStorageKeyName)
+}
+
+export function saveToken (token, storage = localStorage) {
+  if (!token) {
+    storage.removeItem(localStorageKeyName)
+  } else {
+    storage.setItem(localStorageKeyName, token)
+  }
 }
