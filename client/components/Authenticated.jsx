4d66c45f0ed2ebb011de33bc456c3c20d009af1c
Rob <robsc801@gmail.com>
2021-02-15 11:30:56 +1300

    register and sign in lionks working

--- client/components/Authenticated.jsx
+++ client/components/Authenticated.jsx
@@ -1,16 +1,10 @@
 import React from 'react'
-
-// TODO: import a proper isAuthenticated function
-const isAuthenticated = () => true
+import { isAuthenticated } from 'authenticare/client'
 
 export function IfAuthenticated ({ children }) {
-  return isAuthenticated()
-    ? <>{ children }</>
-    : null
+  return isAuthenticated() ? children : null
 }
 
 export function IfNotAuthenticated ({ children }) {
-  return !isAuthenticated()
-    ? <>{ children }</>
-    : null
+  return !isAuthenticated() ? children : null
 }
