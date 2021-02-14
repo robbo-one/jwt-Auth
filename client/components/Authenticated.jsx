4065abc152792a2f7a0ea2092a9f080bd1e46c58
student <student@eda-bc-06.localdomain>
2021-02-15 11:24:54 +1300

    added authenticare function

--- client/components/Authenticated.jsx
+++ client/components/Authenticated.jsx
@@ -1,16 +1,27 @@
-import React from 'react'
+// import React from 'react'
 
-// TODO: import a proper isAuthenticated function
-const isAuthenticated = () => true
+// // TODO: import a proper isAuthenticated function
+// const isAuthenticated = () => true
+
+// export function IfAuthenticated ({ children }) {
+//   return isAuthenticated()
+//     ? <>{ children }</>
+//     : null
+// }
+
+// export function IfNotAuthenticated ({ children }) {
+//   return !isAuthenticated()
+//     ? <>{ children }</>
+//     : null
+// }
+
+import React from 'react'
+import { isAuthenticated } from 'authenticare/client'
 
 export function IfAuthenticated ({ children }) {
-  return isAuthenticated()
-    ? <>{ children }</>
-    : null
+  return isAuthenticated() ? children : null
 }
 
 export function IfNotAuthenticated ({ children }) {
-  return !isAuthenticated()
-    ? <>{ children }</>
-    : null
+  return !isAuthenticated() ? children : null
 }
