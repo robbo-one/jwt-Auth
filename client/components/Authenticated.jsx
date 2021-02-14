323821f1c28ac789d48e637e8cda6ac59c98fbc6
lukas-jj <lukas.jury13@gmail.com>
2021-02-15 11:25:24 +1300

    oneLine

--- client/components/Authenticated.jsx
+++ client/components/Authenticated.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
+import { isAuthenticated } from 'authenticare/client'
 
-// TODO: import a proper isAuthenticated function
-const isAuthenticated = () => true
 
 export function IfAuthenticated ({ children }) {
   return isAuthenticated()
