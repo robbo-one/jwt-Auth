a35515c8012ba87ad73104d7ded46f208e306ca1
student <student@eda-bc-09.localdomain>
2021-02-15 11:30:31 +1300

    got the register button to appear

--- client/components/Authenticated.jsx
+++ client/components/Authenticated.jsx
@@ -1,16 +1,11 @@
 import React from 'react'
-
 // TODO: import a proper isAuthenticated function
-const isAuthenticated = () => true
+import { isAuthenticated } from 'authenticare/client'
 
 export function IfAuthenticated ({ children }) {
-  return isAuthenticated()
-    ? <>{ children }</>
-    : null
+  return isAuthenticated() ? children : null
 }
 
 export function IfNotAuthenticated ({ children }) {
-  return !isAuthenticated()
-    ? <>{ children }</>
-    : null
+  return !isAuthenticated() ? children : null
 }
